9913de490390d755e3c63283fca6d1c02b109181
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-11-01 00:06:34 +0300

    new api implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,76 +2,44 @@ import React, {useEffect, useState} from 'react';
 
 import {Button} from "react-bootstrap";
 
-
 import {Map, TileLayer, Marker, Popup} from "react-leaflet";
-
 
 import {refreshSvg} from "./svg";
 
-interface Neighbor {
-    authenticated: boolean
-    ip: string
-    network_id: number
-    peer_version: number
-    port: number
-    public_key_hash: string
+import config from "./config";
+
+interface Node {
+    address: string;
+    location?: {
+        lat: number;
+        lng: number;
+        country: string;
+        city: string;
+    }
 }
 
-interface Neighbors {
-    sample: Neighbor[],
-    inbound: Neighbor[],
-    outbound: Neighbor[],
+interface Resp {
+    network: string;
+    nodes: Node[]
 }
 
-interface Ip2Loc {
-    "ip": string,
-    "country_code": string,
-    "country_name": string,
-    "region_code": string,
-    "region_name": string,
-    "city": string,
-    "zip_code": string,
-    "time_zone": string,
-    "latitude": number,
-    "longitude": number,
-    "metro_code": number
+const load = (): Promise<Resp> => {
+    return fetch(config.api).then(r => r.json())
 }
-
-const getIPs = async (): Promise<string[]> => {
-    return fetch("https://krypton.talhabulut.com/v2/neighbors")
-        .then(r => r.json())
-        .then((r: Neighbors) => {
-            return Array.from(new Set([
-                ...r.sample.map(x => x.ip),
-                ...r.inbound.map(x => x.ip),
-                ...r.outbound.map(x => x.ip)
-            ].filter(x => x !== "0.0.0.0")));
-        });
-}
-
-const ip2loc = async (ip: string): Promise<Ip2Loc> => {
-    return fetch(`https://freegeoip.app/json/${ip}`)
-        .then(r => r.json())
-}
-
 
 function App() {
     const [loading, setLoading] = useState<boolean>(false);
-    const [locations, setLocations] = useState<Ip2Loc[]>([]);
+    const [network, setNetwork] = useState<string>("")
+    const [nodes, setNodes] = useState<Node[]>([]);
 
-    const load = () => {
-        setLoading(true);
-        setTimeout(() => {
-            getIPs()
-                .then(ips => Promise.all(ips.map(item => ip2loc(item))))
-                .then(r => setLocations(r))
-                .finally(() => setLoading(false));
-        }, 500);
-
-    }
 
     useEffect(() => {
-        load();
+        load()
+            .then(r => {
+                setNetwork(r.network);
+                setNodes(r.nodes);
+            })
+            .finally(() => setLoading(false))
     }, []);
 
     const center = {lat: 31.579396, lng: 13.686786};
@@ -80,11 +48,14 @@ function App() {
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {locations.map((x, i) => {
-            return <Marker position={{lat: x.latitude, lng: x.longitude}}>
+        {nodes.map((x, i) => {
+            if (!x.location) {
+                return null;
+            }
+            return <Marker position={{lat: x.location.lat, lng: x.location.lng}}>
                 <Popup>
-                    {x.country_name} -  {x.city} <br />
-                    {x.ip}
+                    {x.location.country} - {x.location.city} <br/>
+                    {x.address}
                 </Popup>
             </Marker>
         })}
@@ -96,18 +67,24 @@ function App() {
             <div className="content">
                 <div className="list">
                     <div className="list-header">
-                        <div> {loading ? "..." : <span>{locations.length} Nodes</span>}</div>
+                        <div> {loading ? "..." : <span>{nodes.length} Nodes</span>}</div>
                         <div><Button disabled={loading} size="sm" onClick={load}>{refreshSvg}</Button></div>
                     </div>
-                    {locations.map((x, i) => {
+                    {nodes.map((x, i) => {
+                        const nodeLink = `http://${x.address}:20443/v2/info`
                         return <div className="list-item" key={i}>
                             <div className="row-1">
                                 <span className="num">{i + 1}</span>
-                                <span className="ip">{x.ip}</span>
+                                <span className="ip">
+                                    {x.location && <a href={nodeLink} target="_blank" rel="noreferrer">{x.address}</a>}
+                                    {!x.location && <span>{x.address}</span>}
+                                </span>
                             </div>
-                            <div className="row-2">
-                                <span className="country">{x.country_name || "unknown"}</span>
-                            </div>
+                            {x.location && (
+                                <div className="row-2">
+                                    <span className="country">{x.location.country}</span>
+                                </div>
+                            )}
                         </div>
                     })}
                 </div>
---
+++ src/config.example.ts
@@ -0,0 +1,5 @@
+// eslint-disable
+
+export default {
+    api: "http://domain.com"
+}
