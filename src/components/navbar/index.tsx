3ee51cf2874df336b91106070bd7791a7c1013aa
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-11-01 15:21:16 +0300

    navbar style improvements

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -6,18 +6,21 @@ import {openInNewSvg} from "../../svg";
 
 
 const NavBar = () => {
-    return <Navbar className="main-nav-bar">
+    return <Navbar className="main-nav-bar" expand="md">
         <Navbar.Brand href="#home"><img
             src="/logo.png"
             alt="Logo"
             height="30"
             className="d-inline-block align-top"
         /></Navbar.Brand>
-        <Nav className="ml-auto">
-            <Nav.Link href="https://testnet-explorer.blockstack.org" target="_blank">Stacks Explorer {openInNewSvg}</Nav.Link>
-            <Nav.Link href="https://stackstoken.com" target="_blank">Stacks Token {openInNewSvg}</Nav.Link>
-            <Nav.Link href="https://www.blockstack.org" target="_blank">Blockstack {openInNewSvg}</Nav.Link>
-        </Nav>
+        <Navbar.Toggle/>
+        <Navbar.Collapse>
+            <Nav className="ml-auto">
+                <Nav.Link href="https://testnet-explorer.blockstack.org" target="_blank">Stacks Explorer {openInNewSvg}</Nav.Link>
+                <Nav.Link href="https://stackstoken.com" target="_blank">Stacks Token {openInNewSvg}</Nav.Link>
+                <Nav.Link href="https://www.blockstack.org" target="_blank">Blockstack {openInNewSvg}</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
     </Navbar>
 }
 
